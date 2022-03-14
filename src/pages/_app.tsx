0894b1f36844aadd90efb1cf8cb1c7271c3dd5f5
박준민 <Jade@cnspartner.com>
2022-03-14 14:05:04 +0900

    바벨설정 스타일컴포넌트 추가 및 src루트 변경

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import GlobalStyle from '@/assets/styles/globalStyles';
+import { theme } from '@/assets/styles/theme';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+
+function MyApp({ Component, pageProps, router: { route } }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
